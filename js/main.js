1e83b7d43e19082a2ad3b5d8bd994b58a2a14d5b
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-04-30 20:06:04 +0200

    fix JS

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 // VARIABLES
 // ******************************************************
 
-// empty array for numbers
 let numbers = [];
 let userNumbers = [];
 let matchedNumbers = [];
+let timer;
 
 // ******************************************************
 // EVENT LISTENER
@@ -15,7 +15,7 @@ let matchedNumbers = [];
 document.getElementById('start').addEventListener('click', initGame,);
 
 // on click reset the Simon Says game
-document.getElementById('reset').addEventListener('click', restGame,);
+document.getElementById('reset').addEventListener('click', resetGame,);
 
 // ******************************************************
 // FUNCTIONS
@@ -23,13 +23,12 @@ document.getElementById('reset').addEventListener('click', restGame,);
 
 // Simon Says game
 function initGame() {
-    
     generateNumbers(); 
-    setTimeout(checkNumbers, 3000); 
+    setTimer();
 }
 
 // Reset the Simon Says game
-function restGame() {
+function resetGame() {
     numbers = [];
     userNumbers = [];
     matchedNumbers = [];
@@ -38,8 +37,10 @@ function restGame() {
     document.getElementById('userNumbers').innerHTML = '';
     document.getElementById('matchedNumbers').innerHTML = '';
     document.getElementById('result').innerHTML = '';
-
-    initGame();
+    document.getElementById('timer').innerHTML = '';
+    document.getElementById('numbers').innerHTML = '';
+    clearInterval(timer);
+    enableButton();
 }
 
 // generate 5 random numbers
@@ -56,15 +57,24 @@ function generateNumbers() {
     document.getElementById('numbers').innerHTML = numbers;
 }
 
+// check if the numbers inserted by the user are the same of the random numbers
 function checkNumbers() {
        
-    // random numbers are hidden
-    document.getElementById('numbers').classList.add('hidden');
-
-   
     // ask the user to insert 5 numbers
     for (let i = 0; i < 5; i++) {
         let userNumber = parseInt(prompt('Insert a number'));
+        while (isNaN(userNumber) || userNumber < 1 || userNumber > 100 || userNumbers.includes(userNumber)) {
+        if (isNaN(userNumber)) {
+            alert('Please insert a number');
+            userNumber = parseInt(prompt('Insert a number'));
+        } else if (userNumber < 1 || userNumber > 100) {
+            alert('Please insert a number between 1 and 100');
+            userNumber = parseInt(prompt('Insert a number'));
+        } else if (userNumbers.includes(userNumber)) {
+            alert('You have already inserted this number');
+            userNumber = parseInt(prompt('Insert a number'));
+        }
+    }
         userNumbers.push(userNumber);
     }
 
@@ -87,5 +97,47 @@ function checkNumbers() {
     } else
         document.getElementById('matchedNumbers').innerHTML = matchedNumbers;
         document.getElementById('result').innerHTML = 'You have matched ' + matchedNumbers.length + ' numbers';
-        console.log('You have matched in total ' + matchedNumbers.length + ' numbers'); 
+        console.log('You have matched in total ' + matchedNumbers.length + ' numbers');
+    
+    document.getElementById('numbers').innerHTML = numbers;
+
+    // reset the arrays
+    numbers = [];
+    userNumbers = [];
+    matchedNumbers = [];
+
+    // reset the timer
+    clearInterval(timer);
 } 
+
+// set the timer for 30 seconds and then check the numbers
+function setTimer() {
+    let seconds = 30;
+    timer = setInterval(function() {
+        seconds--;
+        document.getElementById('timer').innerHTML = 'Time left: ' + seconds;
+        disableButton();
+        if (seconds == 0) {
+            document.getElementById('timer').innerHTML = 'Time over';
+            // random numbers are hidden
+            document.getElementById('numbers').innerHTML = '';
+            clearInterval(timer);
+            enableButton();
+            setTimeout(function() {
+                checkNumbers();
+            }, 100);
+        }
+    }, 1000);
+}  
+
+// function to dont press button start again after start the game 
+function disableButton() {
+    document.getElementById('start').disabled = true;
+}
+
+// function to press button start again after start the game
+function enableButton() {
+    document.getElementById('start').disabled = false;
+}
+
+
